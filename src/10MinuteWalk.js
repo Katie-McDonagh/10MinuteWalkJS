af4921cfc84d53b3b373c625954015bfebec6762
Katie McDonagh <mcdonagh.katie@outlook.com>
2020-08-11 19:39:13 +0100

    refactor'

--- src/10MinuteWalk.js
+++ src/10MinuteWalk.js
@@ -1,20 +1,22 @@
 function tenMinuteWalk(directions){
   if(directions.length === 10){
+
     var numOfNorth = 0;
-    for(var i=0;i<directions.length;i++){
-      if(directions[i] === "n")
-       numOfNorth++;
-    }
     var numOfSouth = 0;
-      for(var i=0;i<directions.length;i++){
-        if(directions[i] === "s")
-              numOfSouth++;
-       }
+
+    for(var i=0;i<directions.length;i++){
+      if(directions[i] === "n"){
+       numOfNorth++;
+      } else if(directions[i] === "s"){
+      numOfSouth++;
+     }
+    }
       if(numOfNorth === numOfSouth){
         return true
       } else {
         return false
       }
+
     } else {
       return false
     }
