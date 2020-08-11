86490ff360d41bda6abb00dcb6ecbe775df8a7e8
Katie McDonagh <mcdonagh.katie@outlook.com>
2020-08-11 16:41:10 +0100

    red test -returns false when given an array with a length less than 10

--- spec/10MinuteWalkSpec.js
+++ spec/10MinuteWalkSpec.js
@@ -2,4 +2,8 @@ describe ("tenMinuteWalk", function(){
   it ("returns true when given 10 directions, equal amounts of N+S and E+W directions", function(){
     expect(tenMinuteWalk(['w', 's', 'e', 'e', 'n', 'n', 'e', 's', 'w', 'w'])).toEqual(true)
   })
+
+  it ("returns false when given an array with a length less than 10", function(){
+    expect(tenMinuteWalk(['n'])).toEqual(false)
+  })
 });
